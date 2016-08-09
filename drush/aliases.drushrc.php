bac761ec95fa0197211dded22a871922761e2e0c
Moshe Weitzman <weitzman@tejasa.com>
2016-08-09 13:18:40 -0400

    Let folks have local modifications to Drush alisaes.

--- drush/aliases.drushrc.php
+++ drush/aliases.drushrc.php
@@ -5,3 +5,8 @@ $root['root'] = dirname(__DIR__). '/web';
 $aliases['hpc'] = $root + array(
   'uri' => 'http://hpc.massforms8.mass.gov/',
 );
+
+// Adjust alias for local configuration as needed.
+if (file_exists(__DIR__ . '/aliases.local.php')) {
+  include __DIR__ . '/aliases.local.php';
+}

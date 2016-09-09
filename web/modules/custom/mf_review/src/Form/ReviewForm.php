ec5d8c01cc55a3c96e5a68cdabd0a1cabc5b0f99
Tech-Tamer, LLC <tech-tamer@tech-tamer.com>
2016-09-09 13:32:19 -0400

    Enable rich text editing so we can experiment with formatted text in review notes.  Set up text formats and add permissions for same.  Add vertical tabs to workflow to enhance layout.  Customize the view of the submission on the review form.

--- web/modules/custom/mf_review/src/Form/ReviewForm.php
+++ web/modules/custom/mf_review/src/Form/ReviewForm.php
@@ -4,6 +4,7 @@ namespace Drupal\mf_review\Form;
 
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\mf_submission\Entity\Submission;
 
 /**
  * Form controller for Review edit forms.
@@ -16,9 +17,22 @@ class ReviewForm extends ContentEntityForm {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    /* @var $entity \Drupal\mf_review\Entity\Review */
     $form = parent::buildForm($form, $form_state);
-    $entity = $this->entity;
+
+    /* @var $entity \Drupal\mf_review\Entity\Review */
+
+
+    // Disable the submission_id field widget.
+    $form['submission_id']['widget']['#access'] = FALSE;
+
+    // And add a rendered view of the submission.
+    /** @var Submission $submission */
+    $submission = $this->entity->get('submission_id')->entity;
+    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('submission');
+    // TODO: We could customize the display of the submission depending on the current state of the Review.
+    $render_array = $view_builder->view($submission, 'default');
+    $form['submission_id']['rendered'] = $render_array;
+
 
     return $form;
   }
